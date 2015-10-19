90439e6b66f9afb270de8d6693a4f3d2187f7a7f
unsigned6 <yuriivlasiuk@gmail.com>
2015-10-19 11:04:16 +0300

    Remove comments

--- src/main/java/org/forjavaday/skipassproject/App.java
+++ src/main/java/org/forjavaday/skipassproject/App.java
@@ -16,14 +16,14 @@ public class App
     	SkiPass sp3 = sps.createSkiPass();
     	Turnstile turnstile = new Turnstile();
     	turnstile.verify(sp0);
-//    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
+    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
     	turnstile.verify(sp0);
     	turnstile.verify(sp1);
     	turnstile.verify(sp2);
     	turnstile.verify(sp3);
-//    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
+    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
     	turnstile.verify(sp0);
-//    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
+    	sps.summaryStatisticsForPeriodGroupByType(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
     	sps.summaryStatisticsForPeriod(LocalDateTime.now().minusDays(1), LocalDateTime.now().plusDays(1));
     }
 }
